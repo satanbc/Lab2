6045faea6386a59c98546a94d3252c9210997018
satanbc <satanbbc@gmail.com>
2022-04-27 17:31:21 +0300

    1

---
+++ cmake-build-debug/CMakeFiles/Lab2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2.dir/main.cpp.obj"
+  "Lab2.exe"
+  "Lab2.exe.manifest"
+  "Lab2.lib"
+  "Lab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+
 using namespace std;
 void Input(int **array, int MatrixSize){
     bool TrueFalse;
@@ -34,6 +35,59 @@ void Mult(int **array1, int **array2, int **array3, int MatrixSize){
         }
     }
 }
+
+void selectAction() {
+    int **array1, **array2, **array3;
+    int MatrixSize = 0;
+    bool hasMatrix = false;
+
+    while (true) {
+        int button;
+        cin >> button;
+        switch (button) {
+            case 1: {
+                cout << "Input the size of the matrix: ";
+                cin >> MatrixSize;
+
+                cout << "Matrix 1: " << endl;
+                array1 = new int *[MatrixSize];
+                for (int i(0); i < MatrixSize; i++) { array1[i] = new int[MatrixSize]; }
+                Input(array1, MatrixSize);
+
+                cout << "Matrix 2: " << endl;
+                array2 = new int *[MatrixSize];
+                for (int i(0); i < MatrixSize; i++) { array2[i] = new int[MatrixSize]; }
+                Input(array2, MatrixSize);
+                cout << "Press the next button: " << endl;
+
+                hasMatrix = true;
+                break;
+            }
+            case 2: {
+                if (hasMatrix == 1) {
+                    array3 = new int *[MatrixSize];
+                    for (int i(0); i < MatrixSize; i++) { array3[i] = new int[MatrixSize]; }
+
+                    Mult(array1, array2, array3, MatrixSize);
+                    cout << "The result is: " << endl;
+                    Print(array3, MatrixSize);
+
+                    for (int i(0); i < MatrixSize; i++)
+                        delete[] array3[i];
+                    delete[] array3;
+                    hasMatrix = false;
+                }else
+                    cout << "Enter the matrix first!" << endl;
+                break;
+            }
+            case 0: {
+                exit(1);
+            }
+        }
+    }
+}
+
+
 void menu(){
     cout << "-----------------" << endl;
     cout << "1-Input" << endl;
@@ -45,54 +99,6 @@ void menu(){
 
 int main()
 {
-    int **array1, **array2, **array3;
-    int MatrixSize;
-    int button;
-    bool hasMatrix=0;
-
     menu();
-
-    while(1){
-        cin >> button;
-        switch (button){
-            case 1:{
-                cout << "Input the size of the matrix: ";
-                cin >> MatrixSize;
-
-                cout << "Matrix 1: " << endl;
-                array1=new int*[MatrixSize];
-                for(int i(0); i<MatrixSize; i++){array1[i]=new int[MatrixSize];}
-                Input(array1, MatrixSize);
-
-                cout << "Matrix 2: " << endl;
-                array2=new int*[MatrixSize];
-                for(int i(0); i<MatrixSize; i++){array2[i]=new int[MatrixSize];}
-                Input(array2, MatrixSize);
-                cout << "Press the next button: " << endl;
-
-                hasMatrix=1;
-                break;
-            }
-            case 2:{
-                if(hasMatrix==1){
-                    array3=new int*[MatrixSize];
-                    for(int i(0); i<MatrixSize; i++){array3[i]=new int[MatrixSize];}
-
-                    Mult(array1, array2, array3, MatrixSize);
-                    cout << "The result is: " << endl;
-                    Print(array3, MatrixSize);
-
-                    for (int i(0); i<MatrixSize; i++)
-                        delete [] array3[i];
-                    delete [] array3;
-                }
-                else
-                    cout << "Enter the matrix first!" << endl;
-                break;
-            }
-            case 0:{
-                exit(1);
-            }
-        }
-    }
+    selectAction();
 }
